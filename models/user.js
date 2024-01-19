14a118ed8c5b7801d620274ba6bd594da65e9ae8
beluchiigbo <queenbeluchi@gmail.com>
2024-01-19 12:19:08 -0600

    user.js

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const { Schema, model, Types } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: function (e) {
+        return /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(
+          e
+        );
+      },
+    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function (friends) {
+  return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
